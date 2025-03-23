facb1553e8294a76f3beee94c29ded9353ce6d02
hino89 <cendol3409@gmail.com>
2025-03-23 15:18:33 +0700

    hello bro

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -94,6 +94,8 @@
                 left: 50%;
                 transform: translate(-50%, -50%);
                 z-index: -1;
+                overflow-x: hidden;
+                overflow-y: hidden;
             }
         }
     </style>
