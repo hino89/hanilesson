de37093b93a265f06601a4ff97989ba1f0e764a0
hino89 <cendol3409@gmail.com>
2025-04-20 17:40:45 +0700

    Hello mannnnnnnnnnnnnnnll

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,8 +10,8 @@
 
     <style>
         html {
-            width: 100%;
-            height: 100%;
+            width: 100vw;
+            height: 100vh;
             touch-action: none;
             overflow: hidden;
         }
