7217f6e0ccbfb553663743e1bd253dc32aead8a6
hino89 <cendol3409@gmail.com>
2025-05-04 20:34:18 +0700

    testing the waters

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,6 +24,11 @@
             margin: 0;
         }
 
+        *, *::before, *::after {
+            box-sizing: border-box !important;
+            max-width: 100vw !important;
+        }
+
         @media (max-width: 768px) {
             html {
                 width: 100%;
@@ -96,10 +101,7 @@
         }
 
         .materi-image {
-            width: auto;
-            height: auto;
-            max-width: 100%;
-            max-height: 100%;
+            width: 100%;
             border-radius: 10px;
             object-fit: cover;
         }
